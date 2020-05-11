b696baf637de0d1ae2f5aa29f91c856e4ae7fd17
gr523 <57265942+gr523@users.noreply.github.com>
2020-05-12 00:42:10 +0600

    Create KissanimeHalalaiser.user.js

---
+++ KissanimeHalalaiser.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         AutoClickHide
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  KissanimeHalalaiser
+// @author       Tasnim Ferdous
+// @match        https://kissanime.ru/*
+// @match        http://kissanime.ru/*
+// @run-at       document-start
+// @grant        remove
+// ==/UserScript==
+var fast=0,clickhideonly=0,blockall=1; //set your chosen method to 1
+/*
+   Note :
+   1.only blockall works on all pages of kissanime the rest works for the video-playback page only;
+   2.The safest method is clickhideonly;
+   3.fast is similar to clickhideonly but much faster and less safer but safer than blockall;
+
+   suggestion:
+   If your inernet speed is good(6/7Mbps+) at the time of using kissanime just use clickhideonly;
+*/
+
+(function(){
+    if(blockall){
+        hideframe();
+    }
+    else {
+        addhide();
+    }
+})();
+
