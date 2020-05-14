b5593973adf0350ed33b919b4d2a61b517bbaa2c
gr523 <57265942+gr523@users.noreply.github.com>
2020-05-14 20:25:14 +0600

    Update hidemethods.js

--- hidemethods.js
+++ hidemethods.js
@@ -1,37 +1,46 @@
 function addhide(){
+    var url = location.href;
+    var server = url.split("&s=");
+    if(server.length<=1) {return 0;}
 
-    function clickHide(){
-        var x=document.getElementsByClassName("divCloseBut");
-        for(var i=0;i<x.length;i++){
-            var y=x[i].getElementsByTagName("a");
-            for(var j=0;j<y.length;j++){
-                y[j].onclick();
-            }
-        }
+    GM_addStyle('.glx-teaser{display:none !important}');
+
+    if(fast){
+        cleanadframe();
+    }
+    clickHideButton();
+}
+
+
+
+function hideframe(){
+    var url = location.href;
+    var server = url.split("&s=");
+
+    GM_addStyle('.glx-teaser{display:none !important}');
+
+    if(server.length==1 || (server.length>1 &&(server[1].startsWith('beta') || server[1].startsWith('def')))){
+        GM_addStyle('iframe{display:none !important;}');
+    } else {
+        return addhide();
+        alert("Chose beta server if possible in case video doesn't work");
     }
 
-    function cleanclutter(){
-        for(var i="1";i<=20;i++){
-            if(document.getElementById("adsIfrme"+i)){
-                document.getElementById("adsIfrme"+i).remove();
-            }
-        }
-    }
+    if(server.length==1) return;
+    window.addEventListener(
+        'load',function(){
+            document.querySelector(".glx-close").click();
+        },false
+    )
 
-    function cleanadframe(){
-        for(var i=0;i<500;i+=100){
-            setTimeout(function(){cleanclutter();},i);
-        }
-    };
+}
 
-    if(!clickhideonly) cleanadframe();
-
+function clickHideButton(){
     window.addEventListener('load', function() {
-        'use strict';
-        if(document.getElementsByClassName("glx-close")){
-            document.getElementsByClassName("glx-close")[0].click();
+        if(document.querySelector(".glx-close")){
+            document.querySelector(".glx-close").click();;
         }
-        for(var i=0;i<=5000;i+=500){
+        for(var i=0;i<5000;i+=100){
             setTimeout(function() {
                 clickHide();
             }, i);
@@ -40,28 +49,40 @@ function addhide(){
     }, false);
 }
 
-function hideframe(){
-    (function() {
-        'use strict';
-        var url = location.href;
-        var server = url.split("&s=");
 
-        if(server.length==1 || server[1].startsWith('beta') || server[1].startsWith('def')){
-            addcssinHead('iframe{display:none !important;}');
-        } else {
-            return addhide();
-            alert("Chose beta server if possible in case video doesn't work");
-        }
+function hideOnAllpages(){
+    var url = location.href;
+    var server = url.split("&s=");
 
-        window.addEventListener(
-            'load',function(){
-                document.getElementsByClassName("glx-close")[0].click();
-            }
-        )
-    })();
-
-    function addcssinHead(elm){
-        var x=document.getElementsByTagName("head")[0];
-        x.innerHTML+=`<style>${elm}</style>`;
+    if(server.length==1){
+        GM_addStyle('iframe{display:none !important;}');
     }
 }
+
+function addcssinHead(elm){
+    var x=document.getElementsByTagName("head")[0];
+    x.innerHTML+=`<style>${elm}</style>`;
+}
+
+
+
+function clickHide(){
+    var x=document.getElementsByClassName("divCloseBut");
+    for(var i=0;i<x.length;i++){
+        x[i].getElementsByTagName("a")[0].onclick();
+    }
+}
+
+function cleanclutter(){
+    for(var i=1;i<=20;i++){
+        if(document.getElementById("adsIfrme"+i)){
+            document.getElementById("adsIfrme"+i).remove();
+        }
+    }
+}
+
+function cleanadframe(){
+    for(var i=0;i<500;i+=50){
+        setTimeout(function(){cleanclutter();},i);
+    }
+};
